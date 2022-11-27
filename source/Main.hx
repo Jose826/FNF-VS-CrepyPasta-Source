00274deac60be6bad81bc063580ae42d62f9f3b8
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-11-27 17:07:43 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -74,7 +74,7 @@ class Main extends Sprite
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
+		#if android
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		if(fpsVar != null) {
