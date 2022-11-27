6a68719c352851469d0af86da7903b999de5a140
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-11-27 16:47:13 -0300

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -116,6 +116,10 @@ class PauseSubState extends MusicBeatSubstate
 		changeSelection();
 
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
+		
+		#if android
+		addVirtualPad(UP_DOWN, A);
+		#end
 	}
 
 	public static var botthing:Bool = ClientPrefs.gameplaySettings.get('botplay');
